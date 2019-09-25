81401550cca6b7bdb2350de40b0129986a74c881
LinggaPurnamaAlHamdi <55777688+LinggaPurnamaAlHamdi@users.noreply.github.com>
2019-09-25 12:30:29 +0700

    Add files via upload

---
+++ IF1-10118038-Latihan3-Input/src/if1/pkg10118038/latihan3/input/IF110118038Latihan3Input.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118038.latihan3.input;
+
+import java.util.Scanner;
+/**
+ *
+ * @author Lingga
+ * NAMA : Lingga Purnama Al Hamdi
+ * KELAS : IF1
+ * NIM : 10118038
+ * Deskripsi Program : Program ini berisi program yang menampilkan Input
+
+ */
+public class IF110118038Latihan3Input {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.print("Masukkan nama anda: ");
+        Scanner scanner = new Scanner(System.in);
+        
+        String nama = scanner.next();
+        System.out.println("Nama anda adalah "+nama);
+        
+    }
+    
+}
